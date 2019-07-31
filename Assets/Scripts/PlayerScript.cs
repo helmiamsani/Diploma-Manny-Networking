582869d43e9f02c97f9212da5287ea61ef801a7a
Helmi Amsani <48036579+helmiamsani@users.noreply.github.com>
2019-07-31 10:14:09 +1000

    Updated

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,18 +1,83 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerScript : NetworkBehaviour
 {
+    public float movementSpeed = 10.0f;
+    public float rotationSpeed = 10.0f;
+    public float jumpHeight = 2.0f;
+    private bool isGrounded = false;
+    private Rigidbody rigid;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigid = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isLocalPlayer)
+        {
+            HandleInput();
+        }
+    }
+
+    void Move(KeyCode _key)
+    {
+        Vector3 position = rigid.position;
+        Quaternion rotation = rigid.rotation;
+        switch (_key)
+        {
+            case KeyCode.W:
+                position += transform.forward * movementSpeed * Time.deltaTime;
+                break;
+            case KeyCode.S:
+                position += -transform.forward * movementSpeed * Time.deltaTime;
+                break;
+            case KeyCode.A:
+                rotation *= Quaternion.AngleAxis(-rotationSpeed, Vector3.up);
+                break;
+            case KeyCode.D:
+                rotation *= Quaternion.AngleAxis(rotationSpeed, Vector3.up);
+                break;
+            case KeyCode.Space:
+                if (isGrounded)
+                {
+                    rigid.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
+                    isGrounded = false;
+                }
+                break;
+        }
+        rigid.MovePosition(position);
+        rigid.MoveRotation(rotation);
+    }
+
+    void HandleInput()
+    {
+        KeyCode[] keys =
+        {
+            KeyCode.W,
+            KeyCode.S,
+            KeyCode.A,
+            KeyCode.D,
+            KeyCode.Space
+        };
+
+        foreach (var key in keys)
+        {
+            if (Input.GetKey(key))
+            {
+                Move(key);
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision _col)
+    {
+        isGrounded = true;
     }
 }
